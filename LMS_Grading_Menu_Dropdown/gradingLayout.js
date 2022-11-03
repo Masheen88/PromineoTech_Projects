67dd2e41b73aa4fcf6506dcbbe5e9c04de58701f
Matthew Cox <mattdrummerguy@gmail.com>
2022-11-03 08:10:06 -0500

    consolidated code greatly

--- LMS_Grading_Menu_Dropdown/gradingLayout.js
+++
@@ -1,4 +0,0 @@
-let gradeSheetWidth = document.getElementById("id_gradeheader");
-gradeSheetWidth.style.width = "884px";
-
-console.log("gradeSheetWidth:", gradeSheetWidth);
--- LMS_Grading_Menu_Dropdown/newDropDownV2.js
+++ LMS_Grading_Menu_Dropdown/newDropDownV2.js
@@ -1,4 +1,5 @@
 let MySQL = document.getElementById("courseid8");
+
 let javascript = document.getElementById("courseid4");
 let js_Q1_2022 = document.getElementById("courseid32");
 let React = document.getElementById("courseid2");
@@ -8,66 +9,34 @@ let FET_Q1_2022 = document.getElementById("courseid33");
 let front_end_tech = document.getElementById("courseid3");
 let FE_May_2022 = document.getElementById("courseid50");
 
-function HideStuff(hideIt) {
-  //check to see if hideIt has a div inside of it with the attribute tabindex="0"
-  //if it does, then add a class of "open" to it and style display none to hideIt
-  //if it doesn't, then do nothing
-
-  //if hideIt is null
-  if (hideIt == null) {
-    //do nothing
-    return;
-  }
-
-  if (hideIt.querySelector('[tabindex="0"]')) {
-    console.log("Hidden", hideIt);
-    hideIt.querySelector('[tabindex="0"]').classList.add("open");
-    hideIt.style.display = "none";
-  }
-}
-
-console.log("MySQL");
-HideStuff(MySQL);
-console.log("javascript");
-HideStuff(javascript);
-console.log("js_Q1_2022");
-HideStuff(js_Q1_2022);
-console.log("React");
-HideStuff(React);
-console.log("React_Q1_2022");
-HideStuff(React_Q1_2022);
-console.log("Orientation_FE");
-HideStuff(Orientation_FE);
-console.log("FET_Q1_2022");
-HideStuff(FET_Q1_2022);
-console.log("front_end_tech");
-HideStuff(front_end_tech);
-console.log("FE_May_2022");
-// HideStuff(FE_May_2022);
-
-// if div class grademe-mod-name has text Week 3: then add style of display none to the class
-
 let AutoGradedAssignments = [
   "Week 0: Coding Assignment - Submission", //autograded
+  "Week 0: Coding Assignment Submission", //autograded
   "Week 0: Schedule - Submission", //autograded
+  "Week 0: Schedule Submission", //autograded
   "Week 1: Research", //autograded
   "Week 1 Coding Assignment", //autograded
   "Week 1: Career Services Assignment - Submission", //autograded
+  "Week 1: Career Services Assignment Submission -- LinkedIn Profile",
   "Week 1: Career Services - Submission", //autograded
   "Week 1: Coding Assignment Submission", //autograded
   "Week 1 Quiz", //autograded
   "Week 2: Research", //autograded
   "Week 2: Career Services Assignment - Submission", //autograded
+  "Week 2: Career Services Assignment Submission -- GitHub Profile", //autograded
   "Week 3: Research", //autograded
   "Week 3: Research Submission", //autograded
   "Week 3: Career Services Assignment - Submission", //autograded
+  "Week 3: Career Services Assignment Submission -- Elevator Pitch", //autograded
   "Week 4: Research", //autograded
   //   "Week 4 Coding Assignment",
   "Week 5: Research", //autograded
   "Week 4: Quiz", //autograded
   "Week 5: Coding Assignment - Submission", //autograded
+  "Week 5: Coding Assignment Submission", //autograded
   "Week 5: Coding Assignment", //autograded
   "Week 5: Career Services Submission", //autograded
+  "Week 5: Career Services Assignment Submission -- JavaScript Flash Cards", //autograded
   //   "Week 5: Quiz",
   //   "Week 5 Coding Assignment",
   //   "Week 6: Research",
@@ -98,9 +67,12 @@ let AutoGradedAssignments = [
   //   "Week 10 Quiz",
   "Week 11: Research",
   "Week 11: Quiz", //autograded
+  "Week 11: Coding Assignment Submission", //autograded
+  "Week 11: Career Services Submission", //autograded
   //   "Week 11: Coding Assignment",
   "Week 13: Career Services Submission", //autograded
   "Week 13: Career Services - Submission", //autograded
+  "Week 13: Career Services Assignment - Submission",
   "Week 13: Coding Assignment Submission", //autograded
   //   "Week 13: Coding Assignment",
   "Week 13: Coding Assignment - Submission", //autograded
@@ -121,27 +93,46 @@ let AutoGradedAssignments = [
   "Week 17: Career Services Assignment - Submission", //autograded
 ];
 
-for (let i = 0; i < AutoGradedAssignments.length; i++) {
-  console.log(AutoGradedAssignments[i]);
+function HideStuff(hideIt) {
+  //if hideIt is null do nothing else do something.
+  if (hideIt == null) {
+    //do nothing
+    return;
+  }
 
-  //loop through all the divs with the class grademe-mod-name
-  //if the text content of the div is equal to the AutoGradedAssignments[i] then add a class of "matthew" to the parent div
-  let divs = document.querySelectorAll(".grademe-mod-name");
-  for (let j = 0; j < divs.length; j++) {
-    if (divs[j].textContent === AutoGradedAssignments[i]) {
-      divs[j].parentElement.classList.add("matthew");
+  parentElement = hideIt.parentElement;
+  console.log("parentElement:", parentElement);
+
+  //assign a class to the parent element
+  parentElement.classList.add("gradeMeCurriculum");
+
+  //if children in parentElement contain text from AutoGradedAssignments array then hide it
+  for (let i = 0; i < parentElement.children.length; i++) {
+    for (let j = 0; j < AutoGradedAssignments.length; j++) {
+      if (
+        parentElement.children[i].innerText.includes(AutoGradedAssignments[j])
+      ) {
+        parentElement.children[i].style.display = "none";
+      }
     }
   }
-
-  /*
-  //if the parent div has a class of "matthew" then add a style of display none with important tag
-  if (document.querySelector(".matthew")) {
-    let matthew = document.querySelector(".dd-wrap.matthew");
-    console.log("you found a matthew:", matthew);
-    // add a style to matthew after page has fully loaded
-    window.addEventListener("load", function () {
-      matthew.style.display = "none !important";
-    });
-  }
-*/
 }
+
+console.log("MySQL");
+HideStuff(MySQL);
+console.log("javascript");
+HideStuff(javascript);
+console.log("js_Q1_2022");
+HideStuff(js_Q1_2022);
+console.log("React");
+HideStuff(React);
+console.log("React_Q1_2022");
+HideStuff(React_Q1_2022);
+console.log("Orientation_FE");
+HideStuff(Orientation_FE);
+console.log("FET_Q1_2022");
+HideStuff(FET_Q1_2022);
+console.log("front_end_tech");
+HideStuff(front_end_tech);
+console.log("FE_May_2022");
+HideStuff(FE_May_2022);
